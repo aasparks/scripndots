6bfab68a9cd335b7a3ac29d49aa5cc9689453d38
Austin Sparks <aasparks93@gmail.com>
2019-09-01 12:27:53 -0400

    Changed some hotkeys

--- dots/awesome/rc.lua
+++ dots/awesome/rc.lua
@@ -12,7 +12,11 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+-- require("awful.hotkeys_popup.keys")
+-- require("vim")
+-- require("emacs")
+-- require("spacemacs")
+-- require("tmux")
 -- Load Debian menu entries
 local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
@@ -352,9 +356,9 @@ globalkeys = gears.table.join(
               end,
               {description = "lua execute prompt", group = "awesome"}),
     -- Volume control
-    awful.key({ }, "XF86AudioRaiseVolume", function () awful.spawn("amixer -D pulse sset Master 5%+") end, {description = "increase volume", group = "custom"}),
-    awful.key({ }, "XF86AudioLowerVolume", function () awful.spawn("amixer -D pulse sset Master 5%-") end, {description = "decrease volume", group = "custom"}),
-    awful.key({ }, "XF86AudioMute", function () awful.spawn("amixer -D pulse set Master +1 toggle") end, {description = "mute volume", group = "custom"}),
+    awful.key({ }, "XF86AudioRaiseVolume", function () awful.spawn("amixer -D pulse sset Master 5%+") end, {description = "increase volume", group = "volume"}),
+    awful.key({ }, "XF86AudioLowerVolume", function () awful.spawn("amixer -D pulse sset Master 5%-") end, {description = "decrease volume", group = "volume"}),
+    awful.key({ }, "XF86AudioMute", function () awful.spawn("amixer -D pulse set Master +1 toggle") end, {description = "mute volume", group = "volume"}),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"})
@@ -407,52 +411,52 @@ clientkeys = gears.table.join(
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do
-    globalkeys = gears.table.join(globalkeys,
+--for i = 1, 9 do
+  --  globalkeys = gears.table.join(globalkeys,
         -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = awful.screen.focused()
-                        local tag = screen.tags[i]
-                        if tag then
-                           tag:view_only()
-                        end
-                  end,
-                  {description = "view tag #"..i, group = "tag"}),
+   --     awful.key({ modkey }, "#" .. i + 9,
+   --               function ()
+  --                      local screen = awful.screen.focused()
+  --                      local tag = screen.tags[i]
+  --                      if tag then
+  --                         tag:view_only()
+  --                      end
+  --                end,
+  --                {description = "view tag #"..i, group = "tag"}),
         -- Toggle tag display.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = awful.screen.focused()
-                      local tag = screen.tags[i]
-                      if tag then
-                         awful.tag.viewtoggle(tag)
-                      end
-                  end,
-                  {description = "toggle tag #" .. i, group = "tag"}),
+ --       awful.key({ modkey, "Control" }, "#" .. i + 9,
+ --                 function ()
+ --                     local screen = awful.screen.focused()
+ --                     local tag = screen.tags[i]
+ --                     if tag then
+ --                        awful.tag.viewtoggle(tag)
+ --                     end
+ --                 end,
+ --                 {description = "toggle tag #" .. i, group = "tag"}),
         -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:move_to_tag(tag)
-                          end
-                     end
-                  end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
+   --     awful.key({ modkey, "Shift" }, "#" .. i + 9,
+   --               function ()
+   --                   if client.focus then
+   --                       local tag = client.focus.screen.tags[i]
+   --                       if tag then
+   --                           client.focus:move_to_tag(tag)
+   --                       end
+   --                  end
+   --               end,
+   --               {description = "move focused client to tag #"..i, group = "tag"}),
         -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:toggle_tag(tag)
-                          end
-                      end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
-    )
-end
+   --     awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+   --               function ()
+   --                   if client.focus then
+   --                       local tag = client.focus.screen.tags[i]
+   --                       if tag then
+   --                           client.focus:toggle_tag(tag)
+   --                       end
+   --                   end
+   --               end,
+   --               {description = "toggle focused client on tag #" .. i, group = "tag"})
+  --  )
+--end
 
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
