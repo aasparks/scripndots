a7c201805cde0f7c1e4d9412f2753069add5f693
Austin Sparks <aasparks93@gmail.com>
2019-09-26 21:27:27 -0400

    Updated sonic

--- scripts/installers/sonic-lambda.sh
+++ scripts/installers/sonic-lambda.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+set -e
+
+# Installs sonic-pi.
+#
+# Assumptions (to be removed):
+# - Racket is already installed
+# - sonic-pi plugin was installed manually
+#
+
+
 REPO="git@github.com:aasparks/sonic-pi.git"
 # TODO: Write this so that updates won't break it
-SONIC="$HOME/.racket/7.3/pkgs"
+SONIC="$HOME/.racket/7.4/pkgs/sonic-pi"
+
+sudo apt install supercollider-server
 
 git clone $REPO
-cp -r sonic-pi $SONIC
-rm -rf sonic-pi
-sudo rm -f /sonic
-sudo ln -s $SONIC/sonic-pi /sonic
+mkdir -p $SONIC
+rm -rf $SONIC # There's a reason for this but it's stupid
+sudo mv -f sonic-pi $SONIC
 
 sudo raco setup
