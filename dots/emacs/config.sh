1ec409332d4756dd434ed7ff39f552cc54c21034
Austin Sparks <aasparks93@gmail.com>
2020-02-26 10:30:24 -0500

    Added some doom config

--- dots/emacs/config.sh
+++ dots/emacs/config.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+CWD=$(dirname $0)
 
+cp -r $CWD/doom.d ~/.doom.d
+~/.emacs.d/bin/doom refresh
---
+++ dots/emacs/doom.d/config.el
@@ -0,0 +1,9 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq user-full-name "Austin Sparks"
+      user-mail-address "aasparks93@gmail.com")
+
+
+(setq doom-theme 'doom-outrun-electric)
+(setq doom-font (font-spec :family "Inconsolata" :size 21))
