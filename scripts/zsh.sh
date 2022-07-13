7c19321e3ddd97b3a2a4ffe032e2b6fa69eb50cc
Austin Sparks <asparks@Austins-MacBook-Pro.local>
2022-07-13 17:06:12 -0400

    fixed missing oh-my-zsh installer

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+set +x
 
 HERE=$(dirname "$0")
 
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.zsh*
+
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-highlighting
+
+# Copy config
 cp "$HERE"/dots/zshrc ~/.zshrc
+
